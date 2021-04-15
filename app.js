25e764e70f236ace4f65bbef9819fd87fcab41bd
canjie.cai <49980571+MoveAXS@users.noreply.github.com>
2021-04-15 15:17:38 +0800

    update

--- app.js
+++ app.js
@@ -6,26 +6,35 @@ const signdata = qs.signdata
 const headers = qs.headers
 const loginApi = qs.loginApi
 
+login().then(token => {
+    sign(token)
+})
 
-axios.post(loginApi, data, { headers }).then((res) => {
-    if (res && res.data && res.data.data) {
-        console.log('登录成功>>>签到中')
-        const signApi = qs.signApi(res.data.data.token)
-        axios.post(signApi, signdata, { headers }).then((res2) => {
-            if (res2 && res2.data) {
-                console.log(res2.data.code + ','+ res2.data.message)
-                if (res2.data.code === 20000) {
-                    sendEmail('success',res2.data.message)
-                    return
-                }
-                if (res2.data.code === 64032) {
-                    sendEmail('repeat',res2.data.message)
-                    return
-                }
-                sendEmail('faild',res2.data.message)
+function login(){
+    return new Promise((resolve,reject) => {
+        axios.post(loginApi, data, { headers }).then((res) => {
+            if (res && res.data && res.data.data) {
+                resolve(res.data.data.token)
+            }else{
+                reject()
             }
         })
-    }else{
-        console.log("执行失败");
-    }
-})
+    })
+}
+function sign(){
+    const signApi = qs.signApi(token)
+    axios.post(signApi, signdata, { headers }).then((res2) => {
+        if (res2 && res2.data) {
+            console.log(res2.data.code + ','+ res2.data.message)
+            if (res2.data.code === 20000) {
+                sendEmail('success',res2.data.message)
+                return
+            }
+            if (res2.data.code === 64032) {
+                sendEmail('repeat',res2.data.message)
+                return
+            }
+            sendEmail('faild',res2.data.message)
+        }
+    })
+}
