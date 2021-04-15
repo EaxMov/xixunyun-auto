e8647166dda4d5bdcf2a02e3a4d1f0e96f2519a8
canjie.cai <49980571+MoveAXS@users.noreply.github.com>
2021-04-15 17:59:21 +0800

    提交代码

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const axios = require('axios')
-const qs = require('./utils/qs')
-const sendEmail = require('./utils/email') //需要邮件通知请配置CODE和EMAIL，自己发给自己，并且下面注释打开
+const axios = require("axios")
+const qs = require("./utils/qs")
+const sendEmail = require("./utils/email") //需要邮件通知请配置CODE和EMAIL，自己发给自己，并且下面注释打开
 const data = qs.data
 const signdata = qs.signdata
 const headers = qs.headers
 const loginApi = qs.loginApi
 
-login().then(token => {
+login().then((token) => {
     sign(token)
     studentReportInfo(token)
 })
 
 //登录
-function login(){
-    return new Promise((resolve,reject) => {
+function login() {
+    return new Promise((resolve, reject) => {
         axios.post(loginApi, data, { headers }).then((res) => {
             if (res && res.data && res.data.data) {
                 resolve(res.data.data.token)
-            }else{
+            } else {
                 reject()
             }
         })
@@ -25,42 +25,42 @@ function login(){
 }
 
 //签到提交
-function sign(token){
+function sign(token) {
     const signApi = qs.signApi(token)
     axios.post(signApi, signdata, { headers }).then((res) => {
         if (res && res.data) {
-            console.log(res.data.code + ','+ res.data.message)
-            wechetSend('习讯云签到',res.data.message)
+            console.log(res.data.code + "," + res.data.message)
+            wechetSend("习讯云签到", res.data.message)
         }
     })
 }
 
 //日报提交
-function studentReportInfo(token){
+function studentReportInfo(token) {
     const studentReportApi = qs.studentReportApi(token)
-    const studentReportCommitApit = qs.studentReportCommitApit(token)
-    axios.get(studentReportApi).then(res => {
+    const studentReportCommitApi = qs.studentReportCommitApit(token)
+    axios.get(studentReportApi).then((res) => {
         if (res.data.code === 20000) {
-            const {family_name,family_phone} = res.data.data.list[0]
-            const reportForm = qs.reportdata(family_name,family_phone)
-            axios.post(studentReportCommitApit,reportForm).then(res => {
-                console.log(res.data.code + ','+ res.data.message);
-                wechetSend('习讯云日报提交',res.data.message)
+            const { family_name, family_phone } = res.data.data.list[0]
+            const reportForm = qs.reportdata(family_name, family_phone)
+            axios.post(studentReportCommitApi, reportForm).then((res) => {
+                console.log(res.data.code + "," + res.data.message)
+                wechetSend("习讯云日报提交", res.data.message)
             })
         }
     })
 }
 
 //推送微信通知
-function wechetSend(type,msg){
+function wechetSend(type, msg) {
     const params = {
         token: qs.token,
         title: type,
-        content: msg
+        content: msg,
     }
-    axios.get('http://pushplus.hxtrip.com/send',{ params }).then(res => {
-        if(res && res.data && res.data.code === 200){
-            console.log("发送微信推送成功");
+    axios.get("http://pushplus.hxtrip.com/send", { params }).then((res) => {
+        if (res && res.data && res.data.code === 200) {
+            console.log("发送微信推送成功")
         }
     })
 }
--- utils/qs.js
+++ utils/qs.js
@@ -1,21 +1,21 @@
-const qs = require('qs')
+const qs = require("qs")
 const data = qs.stringify({
     account: process.env.ACCOUNT,
     password: process.env.PASSWORD,
     school_id: process.env.SCHOOL_ID,
     request_source: 3,
-    system: '5.1.1',
+    system: "5.1.1",
 })
 const signdata = qs.stringify({
     address: process.env.ADDRESS,
     address_name: process.env.ADDRESS_NAME,
     latitude: process.env.LATITUDE,
-    longitude:process.env.LONGITUDE,
+    longitude: process.env.LONGITUDE,
     remark: 0,
     change_sign_resource: 0,
 })
 
-const reportdata = (family_name,family_phone) => {
+const reportdata = (family_name, family_phone) => {
     return qs.stringify({
         health_type: 1,
         province_id: 0,
@@ -28,21 +28,21 @@ const reportdata = (family_name,family_phone) => {
         family_phone,
         temperature: (36 + Math.random()).toFixed(1),
         safe: [],
-        file: ''
+        file: "",
     })
 }
 
 const headers = {
-    'content-type': 'application/x-www-form-urlencoded',
+    "content-type": "application/x-www-form-urlencoded",
 }
-const loginApi = 'https://api.xixunyun.com/login/api?from=app&version=4.4.9&platform=android'
+const loginApi = "https://api.xixunyun.com/login/api?from=app&version=4.4.9&platform=android"
 const signApi = (token) => {
     return `https://api.xixunyun.com/signin_rsa?token=${token}&from=app&version=4.4.9&platform=android&entrance_year=0&graduate_year=0&school_id=${process.env.SCHOOL_ID}`
 }
 const studentReportApi = (token) => {
     return `https://api.xixunyun.com/health/studentlist?token=${token}&page_no=1&page_size=1`
 }
-const studentReportCommitApit = (token) => {
+const studentReportCommitApi = (token) => {
     return `https://api.xixunyun.com/health/add?token=${token}`
 }
 const mail = process.env.MAIL
@@ -55,9 +55,9 @@ module.exports = {
     loginApi,
     signApi,
     studentReportApi,
-    studentReportCommitApit,
+    studentReportCommitApi,
     headers,
     mail,
     code,
-    token
+    token,
 }
