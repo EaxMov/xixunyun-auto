085d5b7f091abe254af6efe52bcbdaa25b1c71b0
Eax <49980571+EaxMov@users.noreply.github.com>
2022-03-06 10:29:13 +0800

    test: code

--- utils/qs.js
+++ utils/qs.js
@@ -3,12 +3,13 @@ const NodeRSA = require('node-rsa')
 const pubKey =
   'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlYsiV3DsG+t8OFMLyhdmG2P2J4GJwmwb1rKKcDZmTxEphPiYTeFIg4IFEiqDCATAPHs8UHypphZTK6LlzANyTzl9LjQS6BYVQk81LhQ29dxyrXgwkRw9RdWaMPtcXRD4h6ovx6FQjwQlBM5vaHaJOHhEorHOSyd/deTvcS+hRSQIDAQAB'
 const nodersa = new NodeRSA(pubKey)
-  const data = qs.stringify({
-  account: process.env.ACCOUNT,
-  password: process.env.PASSWORD,
-  school_id: process.env.SCHOOL_ID,
-  request_source: 3,
-  system: '5.1.1'
+nodersa.setOptions({ encryptionScheme: 'pkcs1' })
+const data = qs.stringify({
+account: process.env.ACCOUNT,
+password: process.env.PASSWORD,
+school_id: process.env.SCHOOL_ID,
+request_source: 3,
+system: '5.1.1'
 })
 const signdata = qs.stringify({
   address: process.env.ADDRESS,
